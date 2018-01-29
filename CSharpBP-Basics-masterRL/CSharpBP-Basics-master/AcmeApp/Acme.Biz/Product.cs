2af8fb301a4152c2de29cdd9a34ecb3671e2915c
Ryan Laffey <rlaffy@stratusintelligence.com>
2018-01-29 17:16:47 -0600

    continue class

---
+++ CSharpBP-Basics-masterRL/CSharpBP-Basics-master/AcmeApp/Acme.Biz/Product.cs
@@ -0,0 +1,89 @@
+﻿using Acme.Common;
+using static Acme.Common.LoggingService;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Acme.Biz
+{
+    /// <summary>
+    /// Manages product carried in inventory
+    /// </summary>
+    public class Product
+    {
+        
+        public Product()
+        {
+            Console.WriteLine("Product instance created");
+            //this.ProductVendor = new Vendor();
+            this.Category = "Tools";
+        }
+        public Product(int productID, 
+                        string productName,
+                        string description) : this()
+        {
+            this.ProductID = productID;
+            this.ProductName = productName;
+            this.Description = description;
+
+            Console.WriteLine("Product instance has a name:" + 
+                                ProductName);
+        }
+        private string productName;
+
+        public string ProductName
+        {
+            get {
+                var formattedValue = productName?.Trim();
+                return formattedValue;
+            }
+            set { productName = value; }
+        }
+        private string description;
+
+        public string Description
+        {
+            get { return description; }
+            set { description = value; }
+        }
+        private int productID;
+
+        public int ProductID
+        {
+            get { return productID; }
+            set { productID = value; }
+        }
+        private Vendor productVendor;
+
+        public Vendor ProductVendor
+        {
+            get {
+                if (productVendor == null)
+                {
+                    productVendor = new Vendor();
+                } 
+                return productVendor; }
+            set { productVendor = value; }
+        }
+        public string Category { get; set; }
+        public int SequenceNumber { get; set; } = 1;
+
+        public string ProductCode => this.Category + "-" + this.SequenceNumber;
+        public string SayHello()
+        {
+            //var vendor = new Vendor();
+            //vendor.SendWelcomeEmail("Message from Product");
+
+            var emailService = new EmailService();
+            var confirmation = emailService.SendMessage("New Product",
+                this.ProductName, "sales@abc.com");
+
+            var result = LogAction("saying hello");
+
+            return "Hello " + ProductName + " (" + ProductID + "): " + Description;
+        }
+
+    }
+}
---
+++ CSharpBP-Basics-masterRL/CSharpBP-Basics-master/AcmeApp/Tests/Acme.BizTests/ProductTests.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Acme.Biz;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Acme.Biz.Tests
+{
+    [TestClass()]
+    public class ProductTests
+    {
+        [TestMethod()]
+        public void SayHelloTest()
+        {
+            //Arrange
+            var currentProduct = new Product();
+            currentProduct.ProductName = "Saw";
+            currentProduct.ProductID = 1;
+            currentProduct.Description = "15-inch steel blade hand saw";
+            currentProduct.ProductVendor.CompanyName = "ABC Corp";
+            var expected = "Hello Saw (1): 15-inch steel blade hand saw";            //Act
+
+            //Act
+            var actual = currentProduct.SayHello();
+
+            //Assert
+            Assert.AreEqual(actual, expected);
+        }
+        [TestMethod()]
+        public void SayHello_ParameterizedConstructor()
+        {
+            //Arrange
+            var currentProduct = new Product(1, "Saw", 
+                                 "15-inch steel blade hand saw");
+                        
+            var expected = "Hello Saw (1): 15-inch steel blade hand saw";            //Act
+
+            //Act
+            var actual = currentProduct.SayHello();
+
+            //Assert
+            Assert.AreEqual(actual, expected);
+
+        }
+        [TestMethod()]
+        public void Product_Null()
+        {
+            //Arrange
+            Product currentProduct = null;
+            var companyName = currentProduct?.ProductVendor?.CompanyName;
+
+            string expected = null;
+
+            //Act
+            var actual = companyName;
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+
+        }
+        [TestMethod()]  
+        public void ProductName_Format()
+        {
+            //Arrange
+            var currentProduct = new Product();
+            currentProduct.ProductName = "   Steel Hammer   ";
+
+            var expected = "Steel Hammer";
+            //act
+            var actual = currentProduct.ProductName;
+
+            //assert
+            Assert.AreEqual(expected, actual);
+
+         }
+        [TestMethod()]
+
+        public void ProductCode_DefaultValue()
+        {
+            //Arrange
+            var currentProduct = new Product();
+
+            var expected = "Tools-1";
+
+            //Act
+            var actual = currentProduct.ProductCode;
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
